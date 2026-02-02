f0348c8fc04a1988c69ff31b844b1cd870b1842a
cpage-pivotal <corby.page@broadcom.com>
2026-02-02 10:03:31 -0600

    Inital Commit

---
+++ src/main/java/org/tanzu/ssotile/SsoTileApplication.java
@@ -0,0 +1,13 @@
+package org.tanzu.ssotile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoTileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoTileApplication.class, args);
+    }
+
+}
